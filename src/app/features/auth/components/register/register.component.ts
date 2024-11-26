c7928962d54b233d7bb92273b06ad5e065e560e8
TheByronicHero <168966219+Crispykitty@users.noreply.github.com>
2024-11-26 22:11:58 +0200

    Created authentication feature directory structure. Generated standalone register component. Added auth service and set up user model file.

---
+++ src/app/features/auth/components/register/register.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent {
+
+}
