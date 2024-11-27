3403b952a58e5064e0de6c90c6aa4720aaaf00a0
TheByronicHero <corporatemonkies@gmail.com>
2024-11-27 08:14:37 +0200

    added the register path to app.route.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,20 @@
 import { Routes } from '@angular/router';
 import { ExampleCompComponent } from './features/components/example-comp/example-comp.component';
 import { ExamplePageComponent } from './features/pages/example-page/example-page.component';
+import { RegisterComponent } from './features/auth/components/register/register.component';
 export const routes: Routes = [
     {
         path: 'exmp', component: ExampleCompComponent
     },
     {
         path: 'page', component: ExamplePageComponent
+    },
+    {
+        path: 'register', component: RegisterComponent
+    },
+    {
+        path:'',
+        redirectTo: 'register',
+        pathMatch: 'full'
     }
 ];
