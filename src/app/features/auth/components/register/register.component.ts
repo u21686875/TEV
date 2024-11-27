54f0fb9512022482194d855ca327d222ce67e7d8
TheByronicHero <corporatemonkies@gmail.com>
2024-11-27 18:21:14 +0200

    added register.component functionality

--- src/app/features/auth/components/register/register.component.ts
+++ src/app/features/auth/components/register/register.component.ts
@@ -1,12 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import {User} from '../../models/user.model'
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  user: User = {
+    name: '',
+    email: '',
+    username: '',
+    password: '',
+    joinedDate: new Date()
+  };
 
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    this.authService.register(this.user);
+  }
 }
--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import {User} from '../models/user.model'
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
   constructor() { }
+
+  register(user: User) {
+    // Will connect to Supabase later
+    console.log('Registering user:', user);
+  }
+
+  login(email: string, password: string) {
+    // Will connect to Supabase later
+    console.log("Logging in User:", email);
+  }
 }
